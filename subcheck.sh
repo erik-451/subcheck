dc922aebc048f5bd086cbe349c04520277139622
Erik451 <47476901+erik-451@users.noreply.github.com>
2021-12-30 02:08:43 +0100

    Update output

--- subcheck.sh
+++ subcheck.sh
@@ -3,11 +3,11 @@
 if [ $1 == "" ]; then
 
 echo "./subcheck list-domains.txt /folder time|grep {status code}" 
-echo "Usage: ./subcheck domains.txt /index.php 2"
+echo "Usage: ./subcheck domains.txt /index.php"
 fi
 
 input="$1"
   while IFS= read -r line
   do
-    echo "";printf "$line " & curl -s -o /dev/null -I -w "%{http_code}" --connect-timeout "$3" "$line/$2" 
+     echo "";printf "$line$2 " & curl -s -o /dev/null -I -w "%{http_code}" --connect-timeout 1 "$line/$2"
   done < "$input"
